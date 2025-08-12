41fd786272a538e340acc63dab43c58333424572
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-12 09:03:00 +0200

    ci(super-linter): make line lengths warnings instead of errors

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-export default { extends: ["@commitlint/config-conventional"] };
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [1, "always", 100],
+    "body-max-line-length": [1, "always", 100],
+    "footer-max-length": [1, "always", 100],
+  },
+};
