e5caa8ab6e9e4edc82e36fc4b03b39b35729e059
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-12 13:26:24 +0200

    ci(super-linter): fix commitlint config

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,17 @@
-export default {
+const Configuration = {
+  /*
+   * Inherit rules from conventional commits.
+   */
   extends: ["@commitlint/config-conventional"],
+
+  /*
+   * Any rules defined here will override rules from parent.
+   */
   rules: {
     "header-max-length": [1, "always", 100],
     "body-max-line-length": [1, "always", 100],
     "footer-max-length": [1, "always", 100],
   },
 };
+
+export default Configuration;
