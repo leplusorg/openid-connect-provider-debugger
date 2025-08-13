4ae842a0236ee60119fa549f4ff46ae4dda5d6a1
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-13 16:16:08 +0200

    style: remove unnecessary escape for builtin

--- openid-connect-provider-debugger/bump-apk-versions.sh
+++ openid-connect-provider-debugger/bump-apk-versions.sh
@@ -13,19 +13,19 @@ if [ -f /.dockerenv ]; then
 
 	u=$(\apk -u list | \tr '\n' '|')
 
-	\sed -e :a -e '/\\$/N; s/\\\n//; ta' Dockerfile | \grep -o -e 'apk[^\&\;]*add[^\&\;]*' | \grep -o -e '[^ ]*=[^ ]*' | while IFS= \read -r l; do
+	\sed -e :a -e '/\\$/N; s/\\\n//; ta' Dockerfile | \grep -o -e 'apk[^\&\;]*add[^\&\;]*' | \grep -o -e '[^ ]*=[^ ]*' | while IFS= read -r l; do
 		\rm -f .bump.csv.bak
 		p="${l%=*}"
 		v1="${l#*=}"
-		v="$(\echo "|${u}" | \grep -o -e "\|${p}-[^\|]*upgradable from: ${p}-${v1}" || true)"
+		v="$(echo "|${u}" | \grep -o -e "\|${p}-[^\|]*upgradable from: ${p}-${v1}" || true)"
 		if [ -z "${v}" ]; then
 			continue
 		fi
-		n=$(($(\echo -n "${p}" | wc -c) + 2))
-		v2="$(\echo "${v}" | \cut -d ' ' -f 1 | \cut -c ${n}-)"
+		n=$(($(echo -n "${p}" | wc -c) + 2))
+		v2="$(echo "${v}" | \cut -d ' ' -f 1 | \cut -c ${n}-)"
 		if [ -n "${v2}" ] && [ "${v2}" != "${v1}" ]; then
-			\echo "${p} ${v1} -> ${v2}"
-			\echo "${p},${v1},${v2},${p}=${v1},${p}=${v2}" >>.bump.csv.bak
+			echo "${p} ${v1} -> ${v2}"
+			echo "${p},${v1},${v2},${p}=${v1},${p}=${v2}" >>.bump.csv.bak
 		fi
 	done
 
