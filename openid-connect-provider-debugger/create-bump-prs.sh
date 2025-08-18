e82331e201c8e52f2255bd2ca0aed3b2752688b7
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-18 21:28:47 +0200

    ci(bump): fix wait

--- openid-connect-provider-debugger/create-bump-prs.sh
+++ openid-connect-provider-debugger/create-bump-prs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 set -euo pipefail
+IFS=$'\n\t'
 
 cd "$(\dirname "${0}")"
 
@@ -11,14 +11,14 @@ if [ -f .bump.csv.bak ]; then
 	fi
 	csv="$(\cat .bump.csv.bak)"
 	\rm -f .bump.csv.bak
-	while IFS="," read -r p v1 v2 l1 l2; do
+	while IFS=',' read -r p v1 v2 l1 l2; do
 		b="${p}-${v2//[\~\:]/-}"
 		\git switch --create "${b}"
 		\perl -i -p -e "s|\Q${l1}\E|${l2}|g" Dockerfile
 		\git add Dockerfile
 		\git commit -S -m "build(deps): bump ${p} from ${v1} to ${v2}"
 		echo "Please review and push commit in branch \"${b}\". Waiting..."
-		while \git status | \grep -Fq "Your branch is ahead"; do
+		while [ "$(git rev-list --count --left-only '@{u}...HEAD')" -gt 0 ]; do
 			\sleep 1
 		done
 		\gh pr create -f -l build -l dependencies
